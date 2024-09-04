3964fc6ee48793a8cc9c9eb3a9513374036f2511
TalhaTahir-09 <hamzatalhabwp4@gmail.com>
2024-09-04 10:40:53 -0700

    Cleaned up the code

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -50,7 +50,7 @@ export default function Dashboard() {
         <div className="dashboard-chat-orders-container grid p-8">
           <div className="new-products">
             <div className="flex items-center justify-center p-8">
-              <h2 class="about-us-heading avenir-heavy relative mb-8 w-max animate-[fadeLeft_0.5s_ease_forwards] px-8 text-[48px] opacity-0 before:absolute before:-bottom-1 before:left-0 before:block before:h-[5px] before:w-full before:rounded-full before:bg-[#7A83C6] before:content-['']">
+              <h2 className="about-us-heading avenir-heavy relative mb-8 w-max animate-[fadeLeft_0.5s_ease_forwards] px-8 text-[48px] opacity-0 before:absolute before:-bottom-1 before:left-0 before:block before:h-[5px] before:w-full before:rounded-full before:bg-[#7A83C6] before:content-['']">
                 Products
               </h2>
             </div>
@@ -149,52 +149,52 @@ export default function Dashboard() {
             </div>
           </div>
         </div>
-        <div class="about-us-contianer mt-8 p-8 pb-16 pt-0">
-          <div class="mb-12 flex flex-col items-center justify-center text-center">
-            <h2 class="about-us-heading avenir-heavy relative mb-8 w-max animate-[fadeLeft_0.5s_ease_forwards] px-8 text-[48px] opacity-0 before:absolute before:-bottom-1 before:left-0 before:block before:h-[5px] before:w-full before:rounded-full before:bg-[#7A83C6] before:content-['']">
+        <div className="about-us-contianer mt-8 p-8 pb-16 pt-0">
+          <div className="mb-12 flex flex-col items-center justify-center text-center">
+            <h2 className="about-us-heading avenir-heavy relative mb-8 w-max animate-[fadeLeft_0.5s_ease_forwards] px-8 text-[48px] opacity-0 before:absolute before:-bottom-1 before:left-0 before:block before:h-[5px] before:w-full before:rounded-full before:bg-[#7A83C6] before:content-['']">
               About Us
             </h2>
 
-            <p class="avenir-heavy mt-4 animate-[fadeLeft_0.5s_ease_forwards] text-[38px] text-primaryGrey opacity-0">
+            <p className="avenir-heavy mt-4 animate-[fadeLeft_0.5s_ease_forwards] text-[38px] text-primaryGrey opacity-0">
               <i>"Transforming technology into your competitive </i> edge."
             </p>
-            <p class="avenir-light mt-4 animate-[fadeLeft_0.5s_ease_forwards] text-[18px] text-primaryGrey opacity-0">
+            <p className="avenir-light mt-4 animate-[fadeLeft_0.5s_ease_forwards] text-[18px] text-primaryGrey opacity-0">
               Welcome to ConXioN! We specialize in providing top-quality
               products and services tailored to <br /> meet your needs. Our
               mission is to deliver excellence in everything we do.
             </p>
           </div>
 
-          <div class="grid grid-cols-3 gap-8">
-            <div class="dashboard-card flex animate-[fadeInUp_0.5s_ease_1s_forwards] flex-col items-center justify-between rounded-lg bg-[#7A83C6] p-6 text-center text-primaryWhite opacity-0">
-              <div class="icon-container mb-4 mr-4 flex justify-start text-paragraphGrey opacity-70">
-                <i class="fa-solid fa-bullseye text-[70px]"></i>
+          <div className="grid grid-cols-3 gap-8">
+            <div className="dashboard-card flex animate-[fadeInUp_0.5s_ease_1s_forwards] flex-col items-center justify-between rounded-lg bg-[#7A83C6] p-6 text-center text-primaryWhite opacity-0">
+              <div className="icon-container mb-4 mr-4 flex justify-start text-paragraphGrey opacity-70">
+                <i className="fa-solid fa-bullseye text-[70px]"></i>
               </div>
-              <h3 class="avenir-heavy pb-2 text-[28px]">Our Mission</h3>
-              <p class="avenir-medium text-[16px]">
+              <h3 className="avenir-heavy pb-2 text-[28px]">Our Mission</h3>
+              <p className="avenir-medium text-[16px]">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                 Laboriosam eius velit molestiae voluptatem magnam rem qui earum
                 debitis exercitationem impedit.
               </p>
             </div>
-            <div class="dashboard-card flex animate-[fadeInUp_0.5s_ease_1.5s_forwards] flex-col items-center justify-between rounded-lg bg-[#EE7B88] p-6 text-center text-primaryWhite opacity-0">
-              <div class="icon-container mb-4 mr-4 flex justify-start text-paragraphGrey opacity-70">
-                <i class="fa-solid fa-users text-[70px]"></i>
+            <div className="dashboard-card flex animate-[fadeInUp_0.5s_ease_1.5s_forwards] flex-col items-center justify-between rounded-lg bg-[#EE7B88] p-6 text-center text-primaryWhite opacity-0">
+              <div className="icon-container mb-4 mr-4 flex justify-start text-paragraphGrey opacity-70">
+                <i className="fa-solid fa-users text-[70px]"></i>
               </div>
-              <h3 class="avenir-heavy pb-2 text-[28px]">Our Team</h3>
-              <p class="avenir-medium text-[16px]">
+              <h3 className="avenir-heavy pb-2 text-[28px]">Our Team</h3>
+              <p className="avenir-medium text-[16px]">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                 Laboriosam eius velit molestiae voluptatem magnam rem qui earum
                 debitis exercitationem impedit.
               </p>
             </div>
-            <div class="dashboard-card flex animate-[fadeInUp_0.5s_ease_2s_forwards] flex-col items-center rounded-lg bg-[#00B894] p-6 text-center text-primaryWhite opacity-0">
-              <div class="icon-container mb-4 mr-4 flex justify-start text-paragraphGrey opacity-70">
-                <i class="fa-solid fa-envelope text-[70px]"></i>
+            <div className="dashboard-card flex animate-[fadeInUp_0.5s_ease_2s_forwards] flex-col items-center rounded-lg bg-[#00B894] p-6 text-center text-primaryWhite opacity-0">
+              <div className="icon-container mb-4 mr-4 flex justify-start text-paragraphGrey opacity-70">
+                <i className="fa-solid fa-envelope text-[70px]"></i>
               </div>
               <div>
-                <h3 class="avenir-heavy text-[28px]">Contact Us!</h3>
-                <p class="avenir-medium mt-2 text-[16px]">
+                <h3 className="avenir-heavy text-[28px]">Contact Us!</h3>
+                <p className="avenir-medium mt-2 text-[16px]">
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                   Laboriosam eius velit molestiae voluptatem magnam rem qui
                   earum debitis exercitationem impedit.
